2f150c7d65ca04ff0e87233bdb3d742f1085a690
Seungho Jang <sjang1594@gmail.com>
2025-04-25 13:10:51 +0900

    feat: project init & separate the project dir

---
+++ FilmGo/FilmGo/App/SceneDelegate.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let window = UIWindow(windowScene: windowScene)
+
+        let navigationController = UINavigationController(
+            rootViewController: ViewController()
+        )
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        self.window = window
+
+        // [COMMENT] Coordinator & DI Container:
+        // 1. Coordinater 가 있다면, navigationController 및 DIcontainer 주입 -> .start
+        // 2. 없다면, DIContainer 만, Client Coordinator 에 주입 및 전달
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    }
+
+
+}
+
