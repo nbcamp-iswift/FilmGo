b220945b86221ea319f3119d61883b0b1aa5acdd
Seungho Jang <sjang1594@gmail.com>
2025-04-26 19:08:26 +0900

    feat: hide api key with config

--- FilmGo/FilmGo/App/AppConfiguration.swift
+++ FilmGo/FilmGo/App/AppConfiguration.swift
@@ -1,3 +1,10 @@
 import Foundation
 
-final class AppConfiguration {}
+final class AppConfiguration {
+    lazy var apiBaseURL: String = {
+        guard let apiBaseURL = Bundle.main.infoDictionary?["BASE_URL"] as? String else {
+            fatalError("BASE_URL is not defined in Info.plist")
+        }
+        return apiBaseURL
+    }()
+}
