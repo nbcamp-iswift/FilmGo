4dcf28381853993822a480a152bd5521bae948c5
Seungho Jang <sjang1594@gmail.com>
2025-04-26 19:42:47 +0900

    feat: api key mapper
    * mapping necessary key / path to secure api keys.
    * add xcconfig into .gitignore

--- FilmGo/FilmGo/App/AppConfiguration.swift
+++ FilmGo/FilmGo/App/AppConfiguration.swift
@@ -1,10 +1,38 @@
 import Foundation
 
+/// Work as an Environment Config Mapper
 final class AppConfiguration {
-    lazy var apiBaseURL: String = {
-        guard let apiBaseURL = Bundle.main.infoDictionary?["BASE_URL"] as? String else {
-            fatalError("BASE_URL is not defined in Info.plist")
+    private lazy var apiBaseURL: String = getValue(for: "API_BASE_URL")
+
+    private lazy var nowPlayingPath: String = getValue(for: "NOW_PLAYING_PATH")
+
+    private lazy var popularPath: String = getValue(for: "POPULAR_PATH")
+
+    private lazy var topRatedPath: String = getValue(for: "TOP_RATED_PATH")
+
+    private lazy var movieimagePath: String = getValue(for: "MOVIE_IMAGE_PATH")
+
+    var nowPlayingURL: String {
+        apiBaseURL + nowPlayingPath
+    }
+
+    var popularURL: String {
+        apiBaseURL + popularPath
+    }
+
+    var topRatedURL: String {
+        apiBaseURL + topRatedPath
+    }
+
+    func movieImageURL(for movieID: Int) -> String {
+        let path = movieimagePath.replacingOccurrences(of: "$(MOVIE_ID)", with: "\(movieID)")
+        return apiBaseURL + path
+    }
+
+    private func getValue(for key: String) -> String {
+        guard let value = Bundle.main.infoDictionary?[key] as? String else {
+            fatalError("\(key) is not defined in Info.plist")
         }
-        return apiBaseURL
-    }()
+        return value
+    }
 }
