2e79512eb74f9355b2b2f3f5e050d5b3e053e016
Seungho Jang <sjang1594@gmail.com>
2025-04-28 23:05:51 +0900

    feat: define entities

--- FilmGo/FilmGo/Domain/Entity/Movies.swift
+++ FilmGo/FilmGo/Domain/Entity/Movies.swift
@@ -1,8 +1,32 @@
-//
-//  entityMock.swift
-//  FilmGo
-//
-//  Created by SeunghoJang on 4/25/25.
-//
-
 import Foundation
+
+struct Movies {
+    let movieId: Int
+    let poserImage: Data
+    let title: String
+    let star: Double
+    let runningTime: String
+    let releasedYear: Int
+    let genres: [String]
+    let director: String
+    let actors: [String]
+}
+
+struct NowPlayingMovie {
+    let currentPage: Int
+    let totalPages: Int
+    let movies: [Movies]
+}
+
+struct PopularMovies {
+    let currentPage: Int
+    let totalPages: Int
+    let movies: [Movies]
+}
+
+// TODO: Move this to CoreData
+struct SearchMovie {
+    let currentPage: Int
+    let totalPages: Int
+    let movies: [Movies]
+}
