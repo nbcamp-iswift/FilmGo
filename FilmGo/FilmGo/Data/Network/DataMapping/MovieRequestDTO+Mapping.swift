18faa124f1b2e3f841f3f344ac59639caa635eee
Seungho Jang <sjang1594@gmail.com>
2025-04-28 17:44:05 +0900

    feat: define reqDTO & resDTO for APIs

--- FilmGo/FilmGo/Data/Network/DataMapping/MovieRequestDTO+Mapping.swift
+++ FilmGo/FilmGo/Data/Network/DataMapping/MovieRequestDTO+Mapping.swift
@@ -3,5 +3,33 @@ import Foundation
 struct MoviesRequestDTO: Encodable {
     let language: String
     let page: Int
-    let region: String?
+    let region: String
+
+    init(language: String, page: Int, region: String) {
+        self.language = "ko-KR"
+        self.page = page
+        self.region = "KR"
+    }
+}
+
+struct MovieDetailRequestDTO: Encodable {
+    let movieID: Int
+    let language: String
+    let appendToResponse: String?
+
+    init(movieID: Int, appendToResponse: String? = nil) {
+        self.movieID = movieID
+        language = "ko-KR"
+        self.appendToResponse = appendToResponse
+    }
+}
+
+struct MovieCreditsRequestDTO: Encodable {
+    let movieID: Int
+    let language: String
+
+    init (movieID: Int, language: String) {
+        self.movieID = movieID
+        self.language = "ko-KR"
+    }
 }
--- FilmGo/FilmGo/Data/Network/DataMapping/MovieResponseDTO+Mapping.swift
+++ FilmGo/FilmGo/Data/Network/DataMapping/MovieResponseDTO+Mapping.swift
@@ -31,19 +31,6 @@ struct PopularMoviesResponseDTO: Codable {
     }
 }
 
-// MARK: - Top Rated URL <-> Response Data
-struct TopRatedMoviesResponseDTO: Codable {
-    let page: Int
-    let results: [MovieSummaryDTO]
-    let totalPages, totalResults: Int
-
-    enum CodingKeys: String, CodingKey {
-        case page, results
-        case totalPages = "total_pages"
-        case totalResults = "total_results"
-    }
-}
-
 // MARK: - This Shares with Popular ULR & Now Playing URL
 struct MovieSummaryDTO: Codable {
     let adult: Bool
@@ -75,8 +62,159 @@ struct MovieSummaryDTO: Codable {
 }
 
 enum OriginalLanguage: String, Codable {
-    case kr = "kr"
-    case en = "en"
-    case ja = "ja"
-    case te = "te"
+    case kr
+    case en
+    case ja
+    case te
 }
+
+// MARK: -  MovieDetail URL <-> Response Data
+struct MovieDetailResponseDTO: Codable {
+    let adult: Bool
+    let backdropPath: String
+    let belongsToCollection: BelongsToCollection
+    let budget: Int
+    let genres: [Genre]
+    let homepage: String
+    let id: Int
+    let imdbID: String
+    let originCountry: [String]
+    let originalLanguage: OriginalLanguage
+    let originalTitle, overview: String
+    let popularity: Double
+    let posterPath: String
+    let productionCompanies: [ProductionCompany]
+    let productionCountries: [ProductionCountry]
+    let releaseDate: String
+    let revenue, runtime: Int
+    let spokenLanguages: [SpokenLanguage]
+    let status, tagline, title: String
+    let video: Bool
+    let voteAverage: Double
+    let voteCount: Int
+    let videos: Videos?
+    let images: Images?
+
+    enum CodingKeys: String, CodingKey {
+        case adult
+        case backdropPath = "backdrop_path"
+        case belongsToCollection = "belongs_to_collection"
+        case budget, genres, homepage, id
+        case imdbID = "imdb_id"
+        case originCountry = "origin_country"
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case overview, popularity
+        case posterPath = "poster_path"
+        case productionCompanies = "production_companies"
+        case productionCountries = "production_countries"
+        case releaseDate = "release_date"
+        case revenue, runtime
+        case spokenLanguages = "spoken_languages"
+        case status, tagline, title, video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+        case videos, images
+    }
+}
+
+struct BelongsToCollection: Codable {
+    let id: Int
+    let name, posterPath, backdropPath: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, name
+        case posterPath = "poster_path"
+        case backdropPath = "backdrop_path"
+    }
+}
+
+struct Genre: Codable {
+    let id: Int
+    let name: String
+}
+
+struct Images: Codable {
+    let backdrops: [ImageItem]
+    let logos: [ImageItem]
+    let posters: [ImageItem]
+}
+
+struct ImageItem: Codable {
+    let aspectRatio: Double
+    let filePath: String
+    let width: Int
+    let height: Int
+    let iso6391: String?
+    let voteAverage: Double
+    let voteCount: Int
+
+    enum CodingKeys: String, CodingKey {
+        case aspectRatio = "aspect_ratio"
+        case filePath = "file_path"
+        case height
+        case iso6391 = "iso_639_1"
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+        case width
+    }
+}
+
+struct ProductionCompany: Codable {
+    let id: Int
+    let logoPath: String?
+    let name: String
+    let originCountry: String
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case logoPath = "logo_path"
+        case name
+        case originCountry = "origin_country"
+    }
+}
+
+struct ProductionCountry: Codable {
+    let iso3166_1: String
+    let name: String
+
+    enum CodingKeys: String, CodingKey {
+        case iso3166_1 = "iso_3166_1"
+        case name
+    }
+}
+
+struct SpokenLanguage: Codable {
+    let englishName: String
+    let iso639_1: String
+    let name: String
+
+    enum CodingKeys: String, CodingKey {
+        case englishName = "english_name"
+        case iso639_1 = "iso_639_1"
+        case name
+    }
+}
+
+struct Videos: Codable {
+    let results: [VideoItem]
+}
+
+struct VideoItem: Codable {
+    let iso639_1, iso3166_1, name, key: String
+    let site: String
+    let size: Int
+    let type: String
+    let official: Bool
+    let publishedAt, id: String
+
+    enum CodingKeys: String, CodingKey {
+        case iso639_1 = "iso_639_1"
+        case iso3166_1 = "iso_3166_1"
+        case name, key, site, size, type, official
+        case publishedAt = "published_at"
+        case id
+    }
+}
+
+// MARK: - MovieCredit 
