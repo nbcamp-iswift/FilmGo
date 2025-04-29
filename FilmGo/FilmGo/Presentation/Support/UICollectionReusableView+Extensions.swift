867d960d76530e1b5ced5e48ed1b8668da5fd363
KwakDADA <down022@gmail.com>
2025-04-29 15:58:54 +0900

    feat: SectionHeader 구현

---
+++ FilmGo/FilmGo/Presentation/Support/UICollectionReusableView+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  UICollectionReusableView+Extensions.swift
+//  FilmGo
+//
+//  Created by 곽다은 on 4/29/25.
+//
+
+import UIKit
+
+extension UICollectionReusableView {
+    static var identifier: String {
+        String(describing: self)
+    }
+}
