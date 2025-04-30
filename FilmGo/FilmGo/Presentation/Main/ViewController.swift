1711b427b4db5651e3728eaa0d390f94232564ee
youseokhwan <youseokhwan15@gmail.com>
2025-04-30 13:55:45 +0900

    fix: 임시 코드 삭제

--- FilmGo/FilmGo/Presentation/Main/ViewController.swift
+++ FilmGo/FilmGo/Presentation/Main/ViewController.swift
@@ -1,34 +1,7 @@
 import UIKit
-import RxSwift
-import RxCocoa
 
 class ViewController: UIViewController {
-    private let disposeBag = DisposeBag()
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let service = DefaultNetworkService()
-        let repository = DefaultMovieRepository(networkService: service)
-        let moviesSingle = repository.fetchPopularMovies(page: 1)
-
-        moviesSingle
-            .observe(on: MainScheduler.instance)
-            .subscribe { [weak self] result in
-                switch result {
-                case .success(let movies):
-                    let useCase = FetchMovieUseCase(repository: repository)
-                    guard let movieID = movies.movies.first?.movieId else { return }
-                    let vm = DetailViewModel(
-                        movieID: movieID,
-                        fetchMovieUseCase: useCase
-                    )
-                    let vc = DetailViewController(viewModel: vm)
-                    self?.navigationController?.pushViewController(vc, animated: true)
-                case .failure(let error):
-                    break
-                }
-            }
-            .disposed(by: disposeBag)
     }
 }
