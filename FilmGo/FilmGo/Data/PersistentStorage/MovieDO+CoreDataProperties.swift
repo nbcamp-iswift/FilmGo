a4fae058739bb40adaf6d294fea266ce9e39cced
youseokhwan <youseokhwan15@gmail.com>
2025-05-01 23:38:46 +0900

    fix: MovieDO 수정

--- FilmGo/FilmGo/Data/PersistentStorage/MovieDO+CoreDataProperties.swift
+++ FilmGo/FilmGo/Data/PersistentStorage/MovieDO+CoreDataProperties.swift
@@ -9,12 +9,12 @@ public extension MovieDO {
     @NSManaged var genre: [String]?
     @NSManaged var id: Int64
     @NSManaged var posterImage: Data?
-    @NSManaged var posterPath: String
-    @NSManaged var runningTime: String
-    @NSManaged var star: String
-    @NSManaged var title: String
-    @NSManaged var overview: String
-    @NSManaged var director: String
+    @NSManaged var posterPath: String?
+    @NSManaged var runningTime: String?
+    @NSManaged var star: String?
+    @NSManaged var title: String?
+    @NSManaged var overview: String?
+    @NSManaged var director: String?
     @NSManaged var actors: [String]?
     @NSManaged var releasedYear: Int64
 }
@@ -25,14 +25,14 @@ extension MovieDO {
     func toDomain() -> Movie {
         Movie(
             movieId: Int(id),
-            posterImagePath: posterPath,
-            title: title,
-            star: star,
-            runningTime: runningTime,
+            posterImagePath: posterPath ?? "",
+            title: title ?? "",
+            star: star ?? "",
+            runningTime: runningTime ?? "",
             releasedYear: Int(releasedYear),
             genres: genre ?? [],
-            overview: overview,
-            director: director,
+            overview: overview ?? "",
+            director: director ?? "",
             actors: actors ?? []
         )
     }
